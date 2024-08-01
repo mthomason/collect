5f80fcbbdba4c1799d07b6a6dc3f9261473d25c9
EverydayApps <michael@everydayapps.com>
2024-08-01 18:09:01 -0400

    Created new class to hold some misc functions.

---
+++ collect/filepathtools.py
@@ -0,0 +1,45 @@
+# /collect/filepathtools.py
+# -*- coding: utf-8 -*-
+
+import os
+
+class FilePathTools:
+	def get_temp_dir() -> str:
+		temp_dir: str = os.getenv('TMPDIR') or os.getenv('TEMP') or os.getenv('TMP') or '/tmp'
+		if not os.path.exists(temp_dir):
+			temp_dir = '/tmp'
+		if not os.path.exists(temp_dir):
+			raise ValueError("Cannot determine the temporary directory.")
+		return temp_dir
+
+	def get_stable_temp_file_path(prefix: str, unique_id: str, extension: str) -> str:
+		temp_dir: str = get_temp_dir()
+
+		if not unique_id:
+			raise ValueError("unique_id is required.")
+		
+		if not extension:
+			raise ValueError("extension is required.")
+
+		if not extension.startswith('.'):
+			extension = '.' + extension
+
+		unique_hash = hashlib.md5((prefix + unique_id).encode()).hexdigest()
+		stable_filename = f"{prefix}_{unique_hash}{extension}"
+		temp_file_path = os.path.join(temp_dir, stable_filename)
+
+		return temp_file_path
+
+	def get_unique_temp_file_path(prefix: str, extension: str) -> str:
+		temp_dir: str = get_temp_dir()
+
+		if not extension.startswith('.'):
+			extension = '.' + extension
+
+		unique_filename = f"{prefix}_{os.getpid()}_{os.urandom(8).hex()}{extension}"
+		temp_file_path = os.path.join(temp_dir, unique_filename)
+
+		return temp_file_path
+
+if __name__ == "__main__":
+	raise ValueError("This script is not meant to be run directly.")
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # /main.py
 # -*- coding: utf-8 -*-
 
-import os
 from dotenv import load_dotenv
 
-import hashlib
 import uuid
 import markdown
 from io import StringIO
@@ -117,43 +115,6 @@ def search_results_to_markdown(items: list[dict], exclude:list[str] = None) -> s
 		print("No items found or an error occurred.")
 
 	return buffer.getvalue()
-
-def get_temp_dir() -> str:
-	temp_dir: str = os.getenv('TMPDIR') or os.getenv('TEMP') or os.getenv('TMP') or '/tmp'
-	if not os.path.exists(temp_dir):
-		temp_dir = '/tmp'
-	if not os.path.exists(temp_dir):
-		raise ValueError("Cannot determine the temporary directory.")
-	return temp_dir
-
-def get_stable_temp_file_path(prefix: str, unique_id: str, extension: str) -> str:
-	temp_dir: str = get_temp_dir()
-
-	if not unique_id:
-		raise ValueError("unique_id is required.")
-	
-	if not extension:
-		raise ValueError("extension is required.")
-
-	if not extension.startswith('.'):
-		extension = '.' + extension
-
-	unique_hash = hashlib.md5((prefix + unique_id).encode()).hexdigest()
-	stable_filename = f"{prefix}_{unique_hash}{extension}"
-	temp_file_path = os.path.join(temp_dir, stable_filename)
-
-	return temp_file_path
-
-def get_unique_temp_file_path(prefix: str, extension: str) -> str:
-	temp_dir: str = get_temp_dir()
-
-	if not extension.startswith('.'):
-		extension = '.' + extension
-
-	unique_filename = f"{prefix}_{os.getpid()}_{os.urandom(8).hex()}{extension}"
-	temp_file_path = os.path.join(temp_dir, unique_filename)
-
-	return temp_file_path
 
 def search_top_items_from_catagory(category_id: str, ttl: int) -> list[dict[str, any]]:
 	file_name: str = str.join(".", [str.zfill(category_id, 6), "json"])
